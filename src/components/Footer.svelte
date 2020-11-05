88190db4ca481df1f1d33430a9c35ad41494bec9
Ricardo Maia <rsmaia@gmail.com>
2020-11-05 19:32:56 -0300

    Configurações para o ambiente CBMDF

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -9,14 +9,7 @@
 </style>
 
 <div class="row">
-  <div class="col">
-    <a
-      class="github-button"
-      href="https://github.com/ricardomaia"
-      data-size="large"
-      data-show-count="true"
-      aria-label="Follow @ricardomaia on GitHub">@ricardomaia</a>
-  </div>
+  <div class="col" />
   <div class="col powered">
     Powered by
     <a href="https://github.com/ricardomaia/statuspage" target="_blank">
